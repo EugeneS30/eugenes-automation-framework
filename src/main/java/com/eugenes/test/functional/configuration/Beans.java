bad54c5dfbdc59aa2b7168defe51393d64dee99b
EugeneS <ez002622@MSF0795991157F.int.det.wa.edu.au>
2016-05-31 17:32:17 +0800

    removed uneeded classes

--- src/main/java/com/eugenes/test/functional/configuration/Beans.java
+++
@@ -1,9 +0,0 @@
-package com.eugenes.test.functional.configuration;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class Beans {
-    public static final String SHORT_DURATION = "shortDuration";
-}
